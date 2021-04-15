359f222ef68faa4464090fd318ec53f4283aa37a
AndyQA <andrei.melni4uk@gmail.com>
2021-04-15 14:07:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// console.log("My name is Andy");
+//let number = 5;
+//const LeftBorderWight = 5;
+//number = 10;
+//console.log(number);
+"use strict";
+/* const category = "toys";
+ console.log(`https://someurl.com/${category}/5`);
+const user = `Andy`;
+alert(`Привет, ${user}`); */
+/* let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr); */
